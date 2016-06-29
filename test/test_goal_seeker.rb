a8c4a085acaaf907fc06c51412190369c75c4f3d
Fabricio Buzeto <nuk.anime13@gmail.com>
2016-06-28 22:03:49 -0300

    Added some more tests

--- test/test_goal_seeker.rb
+++ test/test_goal_seeker.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'goal_seeker'
 
-class GoalSeekTest < Minitest::Test
+class GoalSeekTest_Integers < Minitest::Test
   def test_same
     assert_equal 0, (GoalSeeker.seek  :start=>0 , :goal=>0, :function=>lambda { |x| x })
   end
@@ -20,8 +20,12 @@ class GoalSeekTest < Minitest::Test
       function: lambda { |x| 2*x })
   end
 
+  def test_stop_after_a_while
+    assert_equal 2, (GoalSeeker.seek  start: 0 , goal: 0, max_cycles:100,
+      function: lambda { |x| x*x -5*x + 6 }) # answers can be 2 or 3
+  end
+
   def test_step_in_the_oposite_direction
     assert_equal -1, (GoalSeeker.seek  start: 0 , goal: -1, max_cycles:10, function: lambda { |x| x })
-
   end
 end
