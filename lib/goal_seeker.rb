e75a7daf5ee6731439e3107b7f7658f1ac3d43dc
Fabricio Buzeto <nuk.anime13@gmail.com>
2016-07-08 10:17:27 -0300

    Refactor methods out

--- lib/goal_seeker.rb
+++ lib/goal_seeker.rb
@@ -61,17 +61,19 @@ class BinarySearchSeeker
     @step = step
     @max_cycles = max_cycles
     @function = function
+    @cycle = 0;
+    define_start_and_end
+  end
+
+  def define_start_and_end
     @start_value = @function.call @start
     if @start_value > @goal
       @end = @start
-      @start = max_cycles*step*(-1)
+      @start = @max_cycles*@step*(-1)
     end
     @start_value = @function.call @start
     @end_value = @function.call @end
-    @cycle = 0;
   end
-
-  def diff_goal(v) (v-@goal).abs end
 
   def calculate
     return @start if @start_value == @goal
@@ -81,14 +83,19 @@ class BinarySearchSeeker
       mid_way = (@start + @end) / 2
       mid_value = @function.call mid_way
       return mid_way if mid_value == @goal
-      if (@start_value - @goal).abs > (@end_value - @goal).abs
-        @start = mid_way
-        @start_value = mid_value
-      else
-        @end = mid_way
-        @end_value = mid_value
-      end
+      update_boundaries mid_way, mid_value
     end while (@start - @end).abs > @step.abs and @cycle < @max_cycles
     mid_way
   end
+
+  def update_boundaries mid_way, mid_value
+    if (@start_value - @goal).abs > (@end_value - @goal).abs
+      @start = mid_way
+      @start_value = mid_value
+    else
+      @end = mid_way
+      @end_value = mid_value
+    end
+  end
+
 end
